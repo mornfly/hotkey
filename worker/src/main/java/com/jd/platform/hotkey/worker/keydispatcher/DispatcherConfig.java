0bed01edb76e5978c557782fd5a339ec60c98461
wangyu106 <wangyu106@jd.com>
2020-08-06 13:51:03 +0800

    io线程与业务线程一一绑定

--- worker/src/main/java/com/jd/platform/hotkey/worker/keydispatcher/DispatcherConfig.java
+++ worker/src/main/java/com/jd/platform/hotkey/worker/keydispatcher/DispatcherConfig.java
@@ -3,6 +3,8 @@ package com.jd.platform.hotkey.worker.keydispatcher;
 import com.jd.platform.hotkey.common.model.HotKeyModel;
 import com.jd.platform.hotkey.worker.keylistener.IKeyListener;
 import com.jd.platform.hotkey.worker.tool.CpuNum;
+import com.lmax.disruptor.dsl.Disruptor;
+import org.jctools.queues.SpscArrayQueue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +12,9 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.*;
 
 /**
  * @author wuweifeng
@@ -34,12 +35,24 @@ public class DispatcherConfig {
      * 队列
      */
     public static BlockingQueue<HotKeyModel> QUEUE = new LinkedBlockingQueue<>(2000000);
+    public static Map<String, Queue<HotKeyModel>> MAPQUEUE = new ConcurrentHashMap<>();
+
+    static {
+        //
+        // https://www.jianshu.com/p/fde38db97318
+        int nowCount = CpuNum.workerCount();
+        int num = 2000000 / nowCount;
+        for (int i = 0; i < nowCount; i++) {
+            Queue<HotKeyModel> spscArrayQueue = new SpscArrayQueue<>(num);
+            MAPQUEUE.put(i + "", spscArrayQueue);
+        }
+    }
 
     @Bean
     public Consumer consumer() {
         int nowCount = CpuNum.workerCount();
         //将实际值赋给static变量
-        if(threadCount != 0) {
+        if (threadCount != 0) {
             nowCount = threadCount;
         }
 
@@ -47,10 +60,15 @@ public class DispatcherConfig {
         for (int i = 0; i < nowCount; i++) {
             KeyConsumer keyConsumer = new KeyConsumer();
             keyConsumer.setKeyListener(iKeyListener);
+            keyConsumer.setQueue(MAPQUEUE.get(i+""));
             consumerList.add(keyConsumer);
 
             threadPoolExecutor.submit(keyConsumer::beginConsume);
         }
         return new Consumer(consumerList);
+    }
+
+    public static void main(String[] args) {
+        Queue<String> spscArrayQueue = new SpscArrayQueue<>(16);
     }
 }
--- worker/src/main/java/com/jd/platform/hotkey/worker/keydispatcher/KeyConsumer.java
+++ worker/src/main/java/com/jd/platform/hotkey/worker/keydispatcher/KeyConsumer.java
@@ -5,6 +5,9 @@ import com.jd.platform.hotkey.worker.keylistener.IKeyListener;
 import com.jd.platform.hotkey.worker.keylistener.KeyEventOriginal;
 
 
+import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+
 import static com.jd.platform.hotkey.worker.keydispatcher.DispatcherConfig.QUEUE;
 import static com.jd.platform.hotkey.worker.tool.InitConstant.totalDealCount;
 
@@ -18,25 +21,36 @@ public class KeyConsumer {
 
     private IKeyListener iKeyListener;
 
+    private Queue<HotKeyModel> queue;
+
+
     public void setKeyListener(IKeyListener iKeyListener) {
         this.iKeyListener = iKeyListener;
     }
 
+    public Queue<HotKeyModel> getQueue() {
+        return queue;
+    }
+
+    public void setQueue(Queue<HotKeyModel> queue) {
+        this.queue = queue;
+    }
+
     public void beginConsume() {
         while (true) {
-            try {
-                HotKeyModel model = QUEUE.take();
-                if (model.isRemove()) {
-                    iKeyListener.removeKey(model, KeyEventOriginal.CLIENT);
-                } else {
-                    iKeyListener.newKey(model, KeyEventOriginal.CLIENT);
-                }
-
-                //处理完毕，将数量加1
-                totalDealCount.increment();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            HotKeyModel model = queue.poll();
+            if (model == null) {
+                continue;
             }
+            if (model.isRemove()) {
+                iKeyListener.removeKey(model, KeyEventOriginal.CLIENT);
+            } else {
+                iKeyListener.newKey(model, KeyEventOriginal.CLIENT);
+            }
+
+            //处理完毕，将数量加1
+            totalDealCount.increment();
+
 
         }
     }
