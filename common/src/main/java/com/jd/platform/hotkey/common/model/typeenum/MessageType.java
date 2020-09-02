cf4614e82893b4e149db5cf911371462f60e5d94
wuweifeng10 <Wu@0396110>
2020-09-02 14:32:55 +0800

    merge

--- common/src/main/java/com/jd/platform/hotkey/common/model/typeenum/MessageType.java
+++ common/src/main/java/com/jd/platform/hotkey/common/model/typeenum/MessageType.java
@@ -8,7 +8,8 @@ public enum MessageType {
     APP_NAME((byte) 1),
     REQUEST_NEW_KEY((byte) 2),
     RESPONSE_NEW_KEY((byte) 3),
-    REQUEST_HIT_COUNT((byte) 7),
+    REQUEST_HIT_COUNT((byte) 7), //命中率
+    REQUEST_HOT_KEY((byte) 8), //热key，worker->dashboard
     PING((byte) 4), PONG((byte) 5),
     EMPTY((byte) 6);
 
