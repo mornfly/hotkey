11e64b68525502c4e0a15fb502f78fb06f52cc83
liyunfeng <tangbohu@didiglobal.com>
2021-08-11 17:26:34 +0800

    同步master代码 js  let 改var

--- dashboard/src/main/resources/static/admin/common/js/key-add.js
+++ dashboard/src/main/resources/static/admin/common/js/key-add.js
@@ -4,6 +4,38 @@ $("#form-add").validate({
 	}
 });
 
+$.ajax({
+	cache : true,
+	type : "POST",
+	url : "/user/info",
+	headers: {
+		"Authorization":getCookie("token")
+	},
+	async : false,
+	error : function(XMLHttpRequest){
+		$.modal.alertError(XMLHttpRequest.responseJSON.msg);
+		var token = getCookie("token");
+		if(XMLHttpRequest.status == 1000 && ( token == "undefined" || token =="")){
+			top.location.href = '/user/login';
+		}
+	},
+	success : function(data) {
+		console.log(data)
+		var role = data.role;
+		if(role === "ADMIN"){
+			$("#apps").val("");
+		}
+		var apps = data.appNames;
+		var appName = data.appName;
+		for (var i = 0; i < apps.length; i++) {
+			var app = apps[i];
+			if(app === appName){
+				$("#apps").val(apps[i]);
+			}
+		}
+	}
+});
+
 function add() {
 	var dataFormJson=$("#form-add").serialize();
 	$.ajax({
@@ -23,44 +55,4 @@ function add() {
 		}
 	});
 }
-
-
-function init() {
-
-	console.log(1);
-	$.ajax({
-		cache : true,
-		type : "POST",
-		url : "/user/info",
-		headers: {
-			"Authorization":getCookie("token")
-		},
-		async : false,
-		error : function(XMLHttpRequest){
-			$.modal.alertError(XMLHttpRequest.responseJSON.msg);
-		},
-		success : function(data) {
-			console.log(data)
-			let role = data.role;
-			if(role === "ADMIN"){
-				$("#apps").append("<option></option>");
-			}
-			let apps = data.appNames;
-			let appName = data.appName;
-			for (let i = 0; i < apps.length; i++) {
-				let app = apps[i];
-				if(app === appName){
-					$("#apps").append("<option selected = selected>" + apps[i] + "</option>");
-				}else{
-					$("#apps").append("<option>" + apps[i] + "</option>");
-				}
-			}
-		}
-	});
-}
-
-$(function(){
-	init();
-})
-
 
